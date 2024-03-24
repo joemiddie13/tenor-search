1ccd0f6c686c99cca6dece2e0607589de333facb
joemiddie13 <joemiddie13@gmail.com>
2024-03-23 21:48:05 -0700

    stretch challenge completed

--- app.js
+++ app.js
@@ -20,31 +20,57 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-// Require tenorjs near the top of the file
-const Tenor = require("tenorjs").client({
-  // Replace with your own key
-  "Key": "AIzaSyCXEB-tv4IF4SGnpp4ydmvH5QKl6UOobBM", // https://tenor.com/developer/keyregistration
-  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
-  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
-});
+// // Require tenorjs near the top of the file
+// const Tenor = require("tenorjs").client({
+//   // Replace with your own key
+//   "Key": "AIzaSyCXEB-tv4IF4SGnpp4ydmvH5QKl6UOobBM", // https://tenor.com/developer/keyregistration
+//   "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+//   "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+// });
 
 
-// Routes
+// // Routes
+// app.get('/', (req, res) => {
+//   // Handle the home page when we haven't queried yet
+//   term = ""
+//   if (req.query.term) {
+//       term = req.query.term
+//   }
+//   // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+//   Tenor.Search.Query(term, "10")
+//       .then(response => {
+//           // store the gifs we get back from the search
+//           const gifs = response;
+//           // pass the gifs as an object into the home page
+//           res.render('home', { gifs })
+//       }).catch(console.error);
+// })
+
+// Refactored Stretch Challenge
+const https = require('https');
+
 app.get('/', (req, res) => {
-  // Handle the home page when we haven't queried yet
-  term = ""
-  if (req.query.term) {
-      term = req.query.term
-  }
-  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
-  Tenor.Search.Query(term, "10")
-      .then(response => {
-          // store the gifs we get back from the search
-          const gifs = response;
-          // pass the gifs as an object into the home page
-          res.render('home', { gifs })
-      }).catch(console.error);
-})
+  const term = req.query.term || '';
+  const limit = 10;
+  const apiKey = 'AIzaSyCXEB-tv4IF4SGnpp4ydmvH5QKl6UOobBM';
+  const apiUrl = `https://tenor.googleapis.com/v2/search?q=${term}&key=${apiKey}&limit=${limit}`;
+
+  https.get(apiUrl, (response) => {
+    let data = '';
+
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      const gifs = JSON.parse(data).results;
+      res.render('home', { gifs });
+    });
+  }).on('error', (err) => {
+    console.error('Error:', err);
+    res.status(500).send('An error occurred while fetching GIFs');
+  });
+});
 
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
